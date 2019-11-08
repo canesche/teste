12d5ab6dcc0ae3d60ddb19e2b39b62d4a394946d
canesche <michael.canesche@gmail.com>
2019-11-08 16:24:28 -0300

    teste

--- gcc/gcc.py
+++ gcc/gcc.py
@@ -15,30 +15,15 @@ class GCCPlugin(Magics):
     
     def __init__(self, shell):
         super(GCCPlugin, self).__init__(shell)
-
         self.argparser = helper.get_argparser()
     
     @staticmethod
     def compile(file_path):
-        subprocess.check_output(
-            [compiler, file_path + ext, "-o", file_path + ".out"], stderr=subprocess.STDOUT)
+        subprocess.check_output([compiler, file_path + ext, "-o", file_path + ".out"], stderr=subprocess.STDOUT)
     
-    def run(self, file_path, timeit=False):
-        if timeit:
-            stmt = f"subprocess.check_output(['{compiler} {file_path}.out'], stderr=subprocess.STDOUT)"
-            output = self.shell.run_cell_magic(
-                magic_name="timeit", line="-q -o import subprocess", cell=stmt)
-        else:
-            output = subprocess.check_output(
-                [file_path + ".out"], stderr=subprocess.STDOUT)
-            output = output.decode('utf8')
-            
-        helper.print_out(output)
-        return None
-    
-    def run_gcc(self, file_path, timeit=False):
+    def run_gcc(self, file_path):
         
-        output = subprocess.check_output([compiler, file_path + ".out"], stderr=subprocess.STDOUT)
+        output = subprocess.check_output([file_path + ".out"], stderr=subprocess.STDOUT)
         output = output.decode('utf8')
             
         helper.print_out(output)
@@ -58,7 +43,7 @@ class GCCPlugin(Magics):
                 f.write(cell)
             try:
                 self.compile(file_path)
-                output = self.run_gcc(file_path, timeit=args.timeit)
+                output = self.run_gcc(file_path)
             except subprocess.CalledProcessError as e:
                 helper.print_out(e.output.decode("utf8"))
                 output = None
