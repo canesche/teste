24916f5eb1692a1003239d0ab040dd9d57facfd4
canesche <michael.canesche@gmail.com>
2019-11-11 07:51:50 -0300

    teste

--- v1/v1.py
+++ v1/v1.py
@@ -34,7 +34,7 @@ class NVCCPlugin(Magics):
         helper.print_out(output)
     
     def run_nvprof(self, file_path):
-        output = subprocess.check_output(["nvprof", file_path + ".out", "--metrics sm_efficiency"], stderr=subprocess.STDOUT)
+        output = subprocess.check_output(["nvprof", file_path + ".out", "--metrics sm_efficiency dram_utilization"], stderr=subprocess.STDOUT)
         output = output.decode('utf8')
         helper.print_out(output)
 
