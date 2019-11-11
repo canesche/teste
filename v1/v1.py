74531514e426b0e3d0c0529397bc5b16fd36e78f
canesche <michael.canesche@gmail.com>
2019-11-11 08:47:43 -0300

    teste

--- v1/v1.py
+++ v1/v1.py
@@ -34,7 +34,8 @@ class NVCCPlugin(Magics):
         helper.print_out(output)
     
     def run_nvprof(self, file_path):
-        output = subprocess.check_output(["nvprof", "--metrics all", "./" + file_path + ".out"], stderr=subprocess.STDOUT)
+        args = ["nvprof", "./" + file_path + ".out", "--metrics all"]
+        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
         output = output.decode('utf8')
         helper.print_out(output)
 
