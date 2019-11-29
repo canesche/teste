0672dbca5184478e7c2f35b0b6de426644071290
canesche <michael.canesche@gmail.com>
2019-11-29 09:44:10 -0300

    adding verilog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.3',
     author='Andrei Nechaev, M. Canesche',
     author_email='lyfaradey@yahoo.com, michael.canesche@gmail.com',
-    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'c.c', 'cpp.cpp', 'common.helper'],
+    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'c.c', 'cpp.cpp', 'verilog.verilog', 'common.helper'],
     url='htpps://github.com/andreinechaev/nvcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA C/C++, GCC code',
