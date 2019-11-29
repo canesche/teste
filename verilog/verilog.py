1b4583664f1216c88dbc0794295d9dd5fa89a1f6
canesche <michael.canesche@gmail.com>
2019-11-29 10:10:29 -0300

    adding verilog

--- verilog/verilog.py
+++ verilog/verilog.py
@@ -7,7 +7,7 @@ from IPython.core.magic import Magics, cell_magic, magics_class
 from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
 from common import helper
 
-compiler = './verilog/iverilog'
+compiler = '/content/teste/verilog/iverilog'
 ext = '.v'
 
 @magics_class
